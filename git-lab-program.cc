f9477d80fd725aac692e3a9006e866716a7b7b44
Payton VanCamp <pv674418@ohio.edu>
2020-05-15 21:43:26 -0400

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Payton VanCamp
+ *        Date: 5/15/2020
+ * Description: Git Lab Program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
